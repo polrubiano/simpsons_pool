6d0f92990b52f5f84bf9770c9c0627d0ae418839
Diego Hernandez Rubiano <pol_rubiano@yahoo.es>
2025-10-21 18:42:46 +0200

    fixed extension classes for characters, episodes and locations

--- app/src/main/java/pol/rubiano/simpsonspool/features/locations/domain/models/LocationExt.kt
+++ app/src/main/java/pol/rubiano/simpsonspool/features/locations/domain/models/LocationExt.kt
@@ -1,9 +1,9 @@
 package pol.rubiano.simpsonspool.features.locations.domain.models
 
-import pol.rubiano.simpsonspool.features.locations.data.apimodels.LocationApiModel
+import pol.rubiano.simpsonspool.features.locations.data.local.entities.LocationEntity
 
-fun Location.toApiModel(): LocationApiModel {
-    return LocationApiModel(
+fun Location.toEntity(): LocationEntity {
+    return LocationEntity(
         id = id,
         name = name,
         imagePath = imagePath,
